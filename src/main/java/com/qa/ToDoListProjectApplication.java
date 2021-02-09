c532c2df063a37799bbb618d2a9ca353c826a33e
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-02-09 05:21:58 +0000

    inital setup

--- src/main/java/com/qa/ToDoListProjectApplication.java
+++ src/main/java/com/qa/ToDoListProjectApplication.java
@@ -2,12 +2,16 @@ package com.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ToDoListProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoListProjectApplication.class, args);
+		ApplicationContext beanBag = SpringApplication.run(ToDoListProjectApplication.class, args);
+		
+		Object server = beanBag.getBean("serverStart", String.class);
+		System.out.println("Server Start Time" + " " + server);
 	}
 
 }
