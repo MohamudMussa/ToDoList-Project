d060c03557dee1e530cdd38e1d494e84c8310135
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-02-09 06:04:31 +0000

    sql information

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -1,1 +1,17 @@
+INSERT INTO HOUSE_DOMAIN (address) VALUES 
+('202 Drayton Gardens'),
+('56 North Promenade'),
+('48  Holburn Lane'),
+('60  Botley Road'),
+('128  Stroude Road'),
+('126  Friar Street'),
+('111  Fordham Rd'),
+('84  Rowland Rd');
 
+INSERT INTO CAT_DOMAIN (name, category, rank, completed, my_house_id) VALUES 
+('Buy Banana', 'Shopping', 2, false,1),
+('Football', 'Sport', 3, true,2),
+('Buy Phone', 'Shopping', 2, false,3),
+('Clean', 'To Do', 3, true,4),
+('Run', 'Health', 2, false,5),
+('Finish Report', 'Work', 3, true,6);
