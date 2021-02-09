1c29d5066d0d2694edbcfdc9f82fa13f59aa2115
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-02-09 16:27:29 +0000

    INITALtest

--- src/test/java/com/qa/rest/TaskControllerIntegrationTest.java
+++ src/test/java/com/qa/rest/TaskControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.qa.rest;
 
-
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,87 +47,84 @@ public class TaskControllerIntegrationTest {
 
 	}
 
-	//read one task test
+	// read one task test
 	@Test
 	public void readTask() throws Exception {
-		
-        // EXPECTED INFO BACK WHICH WILL BE JSON
-        TaskDTO expectedResults = new TaskDTO(1L, "Buy Banana", "Shopping", 2, false);
-        
 
-        // this sets up the request
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.GET,
-                "http://localhost:8080/task/read/" + ID);
+		// EXPECTED INFO BACK WHICH WILL BE JSON
+		TaskDTO expectedResults = new TaskDTO(1L, "Buy Banana", "Shopping", 2, false);
 
-        // CHECK STATUS THAT YOU GET
-        ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();
+		// this sets up the request
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.GET,
+				"http://localhost:8080/task/read/" + ID);
 
-        // CHECK IF YOU GET ONE task
-        ResultMatcher matchContent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResults));
-        // PERFORM THE ABOVE
-        this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
-        
+		// CHECK STATUS THAT YOU GET
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();
+
+		// CHECK IF YOU GET ONE task
+		ResultMatcher matchContent = MockMvcResultMatchers.content()
+				.json(jsonifier.writeValueAsString(expectedResults));
+		// PERFORM THE ABOVE
+		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
+
 	}
-	
-	
-	//creating task test
+
+	// creating task test
 	@Test
 	public void create() throws Exception {
-        // EXPECTED INFO BACK WHICH WILL BE JSON
+		// EXPECTED INFO BACK WHICH WILL BE JSON
 
-        TaskDomain contentBody = new TaskDomain("Help Mum", "Helping", 5, false, null);
-        TaskDTO expectedResults = mapToDTO(contentBody); // EXPECTED RESULT
-        expectedResults.setId(7L);
+		TaskDomain contentBody = new TaskDomain("Help Mum", "Helping", 5, false, null);
+		TaskDTO expectedResults = mapToDTO(contentBody); // EXPECTED RESULT
+		expectedResults.setId(7L);
 
-        // this sets up the request
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-                .request(HttpMethod.POST, "http://localhost:8080/task/create").contentType(MediaType.APPLICATION_JSON)
-                .content(jsonifier.writeValueAsString(contentBody)).accept(MediaType.APPLICATION_JSON);
+		// this sets up the request
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+				.request(HttpMethod.POST, "http://localhost:8080/task/create").contentType(MediaType.APPLICATION_JSON)
+				.content(jsonifier.writeValueAsString(contentBody)).accept(MediaType.APPLICATION_JSON);
 
-        // CHECK STATUS THAT YOU GET
-        ResultMatcher matchStatus = MockMvcResultMatchers.status().isCreated();
+		// CHECK STATUS THAT YOU GET
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isCreated();
 
-        // CHECK IF YOU CREATED TASK
-        ResultMatcher matchConent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResults));
+		// CHECK IF YOU CREATED TASK
+		ResultMatcher matchConent = MockMvcResultMatchers.content().json(jsonifier.writeValueAsString(expectedResults));
 
-        // PERFORM THE ABOVE
-        this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchConent);
+		// PERFORM THE ABOVE
+		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchConent);
 	}
-	
 
-	//updating task test
+	// updating task test
 	@Test
 	public void update() throws Exception {
-		
-		
 
 	}
 
-	//delete task test successful
+	// delete task test successful
 	@Test
 	public void delete() throws Exception {
-		
-        // this sets up the request
-        // we are going to delete ID 2
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.DELETE,
-                "http://localhost:8080/task/delete/" + 2);
 
-        // CHECK STATUS THAT YOU GET
-        ResultMatcher matchStatus = MockMvcResultMatchers.status().isNoContent();
+		// this sets up the request
+		// we are going to delete ID 2
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.DELETE,
+				"http://localhost:8080/task/delete/" + 2);
 
-        // PERFORM THE ABOVE
-        this.mock.perform(mockRequest).andExpect(matchStatus);
+		// CHECK STATUS THAT YOU GET
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isNoContent();
+
+		// PERFORM THE ABOVE
+		this.mock.perform(mockRequest).andExpect(matchStatus);
 
 	}
-	//delete task test exception
+
+	// delete task test exception
 	@Test
 	public void deleteInternalError() throws Exception {
 
-            MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.DELETE,
-                    "http://localhost:8080/task/delete/" + 10);
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.request(HttpMethod.DELETE,
+				"http://localhost:8080/task/delete/" + 10);
 
-            ResultMatcher nonMatch = MockMvcResultMatchers.status().isInternalServerError();
-            this.mock.perform(mockRequest).andExpect(nonMatch);
+		ResultMatcher nonMatch = MockMvcResultMatchers.status().isInternalServerError();
+		this.mock.perform(mockRequest).andExpect(nonMatch);
 
 	}
 
--- src/test/java/com/qa/services/TaskServiceUnitTest.java
+++ src/test/java/com/qa/services/TaskServiceUnitTest.java
@@ -1,5 +1,63 @@
 package com.qa.services;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.qa.persistence.domain.TaskDomain;
+import com.qa.persistence.dto.TaskDTO;
+import com.qa.persistence.repos.TaskRepo;
+
+@SpringBootTest
 public class TaskServiceUnitTest {
 
+	@MockBean // @Mock
+	private ModelMapper MockMapper;
+	@MockBean // @Mock
+	private TaskRepo mockRepo;
+
+	@Autowired // @injectMocks
+	private TaskService service;
+	
+//	CRUD
+	
+	
+	//CREAD
+	
+	@Test
+	public void create() {
+
+	}
+	
+//	READ ONE
+	
+	
+	
+	@Test
+	public void readTask() {
+
+	}
+	
+//	READ ALL
+	@Test
+	public void readAll() {
+
+	}
+	
+	
+//	DELETE
+	@Test
+	public void delete() {
+
+	}
+	
 }
