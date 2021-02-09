a4e5880e52b596e02928aa51c98c24d430d7fb38
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-02-09 11:15:04 +0000

    Update TaskControllerIntegrationTest.java

--- src/test/java/com/qa/rest/TaskControllerIntegrationTest.java
+++ src/test/java/com/qa/rest/TaskControllerIntegrationTest.java
@@ -1,5 +1,76 @@
 package com.qa.rest;
 
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.persistence.domain.TaskDomain;
+import com.qa.persistence.dto.TaskDTO;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Sql(scripts = { "classpath:scheme-test.sql",
+		"classpath:data-test.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles(profiles = "test")
 public class TaskControllerIntegrationTest {
+
+	@Autowired
+	private MockMvc mock;
+	@Autowired
+	private ModelMapper mapper;
+	@Autowired
+	private ObjectMapper jsonifier;
+
+	private final int ID = 1;
+
+	private TaskDTO mapToDTO(TaskDomain model) {
+		return this.mapper.map(model, TaskDTO.class);
+	}
+
+	// READ ALL CATS
+	@Test
+	public void readAll() throws Exception {
+
+	}
+
+	@Test
+	public void readCat() throws Exception {
+
+	}
+
+	@Test
+	public void create() throws Exception {
+
+	}
+
+	@Test
+	public void update() throws Exception {
+
+	}
+
+	@Test
+
+	public void delete() throws Exception {
+
+	}
+
+	@Test
+	public void deleteInternalError() throws Exception {
+
+	}
 
 }
