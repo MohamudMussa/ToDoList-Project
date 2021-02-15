127832033b82c40b3913f2beb91af67d14bc9e08
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-02-15 03:13:51 +0000

    Formatted Document and Cleaned HTML

--- src/test/java/com/qa/selenium/AutomatedTesting.java
+++ src/test/java/com/qa/selenium/AutomatedTesting.java
@@ -18,46 +18,50 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
+
 public class AutomatedTesting {
 
 	private static WebElement targ;
 
 	private static WebDriver driver;
-
+	
 	private static ExtentReports report;
-
+	
 	private static ExtentTest test;
-
+	
 	IndexPage website = PageFactory.initElements(driver, IndexPage.class);
 	ToDoListTest ToDoListTest = PageFactory.initElements(driver, ToDoListTest.class);
 	ToDoTaskTest ToDoTaskTest = PageFactory.initElements(driver, ToDoTaskTest.class);
+
 
 	// TODO WEBSITE
 	private static String URL = "http://localhost:8080/";
 
 	@BeforeAll
 	public static void setup() {
-
-		// extent report
+		
+		
+		//extent report
 		report = new ExtentReports("target/reports/ToDoListReport.html", true);
-
-		// driver
+		
+		//driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chrome/chromedriver.exe");
 
 		ChromeOptions config = new ChromeOptions();
-
-		// hides chrome
+		
+		//hides chrome
 		config.setHeadless(true);
 
 		driver = new ChromeDriver(config);
+	
 
 	}
-
+	
 	@AfterEach
 	public void endTest() {
-		// end report after each test
+		//end report after each test
 		report.endTest(test);
-
+	
 	}
 
 	@Test
@@ -75,66 +79,75 @@ public class AutomatedTesting {
 		assertEquals("ToDolist by Mohamud Mussa", driver.getTitle());
 	}
 
-	// READ TASK
-	@Test
-	public void readAllTasks() throws InterruptedException {
-
-		test = report.startTest("READ TASK TEST");
-
-		// GIVEN THAT I CAN ACCES MY TO DO WEBSITE
-		driver.get(URL);
-
-		// WHEN THE WEBSITE LOADS UP
-
+	//READ TASK
+    @Test
+    public void readAllTasks() throws InterruptedException {
+    	
+    	test = report.startTest("READ TASK TEST");
+    	
+    	//GIVEN THAT I CAN ACCES MY TO DO WEBSITE
+        driver.get(URL);
+        
+        
+        //WHEN THE WEBSITE LOADS UP
+        
 		Integer current = driver.findElements(By.xpath("//*[@id=\"taskID\"]/p")).size();
 
-		// THEN I SHOULD BE ABLE TO SEE MY TASKS
-
-		Integer expected = current;
-
-		// ASSERTION
-
-		if (expected.equals(current)) {
+        
+        //THEN I SHOULD BE ABLE TO SEE MY TASKS
+       
+        Integer expected = current;
+        
+        // ASSERTION
+		
+		
+		if(expected.equals(current)) {
 			test.log(LogStatus.PASS, "Tasks have loaded");
 		} else {
 			test.log(LogStatus.FAIL, "tasks have not loaded");
 		}
-
+		
 		assertEquals(expected, current);
-	}
-
-	// READ LIST
-	@Test
-	public void readAllList() throws InterruptedException {
-
-		test = report.startTest("READ LIST TEST");
-
-		// GIVEN THAT I CAN ACCES MY TO DO WEBSITE
-		driver.get(URL);
-
-		// WHEN THE WEBSITE LOADS UP
-
+    }
+    
+    
+	//READ LIST
+    @Test
+    public void readAllList() throws InterruptedException {
+    	
+    	test = report.startTest("READ LIST TEST");
+    	
+    	//GIVEN THAT I CAN ACCES MY TO DO WEBSITE
+        driver.get(URL);
+        
+        
+        //WHEN THE WEBSITE LOADS UP
+        
 		Integer current = driver.findElements(By.xpath("//*[@id=\"todoID\"]/p")).size();
 
-		// THEN I SHOULD BE ABLE TO SEE MY TASKS
-
-		Integer expected = current;
-
-		// ASSERTION
-
-		if (expected.equals(current)) {
+        
+        //THEN I SHOULD BE ABLE TO SEE MY TASKS
+       
+        Integer expected = current;
+        
+        // ASSERTION
+		
+		
+		if(expected.equals(current)) {
 			test.log(LogStatus.PASS, "Lists have loaded");
 		} else {
 			test.log(LogStatus.FAIL, "List have not loaded");
 		}
-
+		
 		assertEquals(expected, current);
-	}
+    }
 
-	// CREATE TASK TEST
+
+	
+	//CREATE TASK TEST
 	@Test
 	public void createATask() throws InterruptedException {
-
+		
 		test = report.startTest("CREATE TASK TEST");
 
 		// GIVEN: That I can access the website
@@ -149,7 +162,7 @@ public class AutomatedTesting {
 		Integer initalcount = driver.findElements(By.xpath("//*[@id=\"taskID\"]/p")).size();
 
 		// THEN: I should get input options AND click on the create button
-		ToDoTaskTest.createTask("A New Task", "Shopping", "2", "false", "1");
+		ToDoTaskTest.createTask("A New Task", "Shopping", "2", "false", "1");	
 
 		// THEN: I should be able to see a new task in my list
 
@@ -158,22 +171,25 @@ public class AutomatedTesting {
 		Integer result = initalcount + 1;
 
 		// ASSERTION
-
-		if (result.equals(expectedCount)) {
+		
+		
+		if(result.equals(expectedCount)) {
 			test.log(LogStatus.PASS, "Task Was Created");
 		} else {
 			test.log(LogStatus.FAIL, "user was not created");
 		}
-
+		
 		assertEquals(expectedCount, result);
+		
+		
 
 	}
 
-	// UPDATE TASK
+	//UPDATE TASK
 	@Test
 	public void updateTask() throws InterruptedException {
-
-		// report name
+		
+		//report name
 		test = report.startTest("UPDATE TASK TEST");
 
 		// GIVEN: That I can access the website
@@ -188,19 +204,21 @@ public class AutomatedTesting {
 
 		targ = driver.findElement(By.xpath("//*[@id=\"task\"]/p[1]"));
 		targ.getText();
-
+		
+		
 		// THEN: I update a LIST using the new information
-
+		
 		ToDoTaskTest.updateTask("1", "Buy Rice", "Work", "3", "false", "1");
-
-		String expectedUpdateTask = "A New Task";
+		
+		String expectedUpdateTask = "Buy Rice";
 
 		// THEN: I should be able to see a new up to date List name
 		WebElement targtwo = driver.findElement(By.xpath("//*[@id=\"task\"]/p[1]"));
-
+		
+		
 		String newValue = targtwo.getText(); // "Buy Rice"
-
-		if (expectedUpdateTask.equals(newValue)) {
+		
+		if(expectedUpdateTask.equals(newValue)) {
 			test.log(LogStatus.PASS, "Task Was Updated");
 		} else {
 			test.log(LogStatus.FAIL, "user was not Updated");
@@ -210,18 +228,21 @@ public class AutomatedTesting {
 		assertEquals(expectedUpdateTask, newValue);
 
 	}
-
-	// DELETE A TASK
+	
+	
+	//DELETE A TASK
 	@Test
 	public void deleteTask() throws InterruptedException {
-
-		// report name
+		
+		//report name
 		test = report.startTest("DELETE TASK TEST");
 
+
+		
 		// GIVEN: That I can access the website
 
 		driver.get(URL);
-
+		
 		// THEN I WANT TO CHECK THE INITAL VALUE AMOUNT OF LIST ITEMS
 
 		Integer deleteTaskBefore = driver.findElements(By.xpath("//*[@id=\"todoID\"]/p")).size();
@@ -232,17 +253,18 @@ public class AutomatedTesting {
 
 		// THEN: I create a LIST using information
 
-		ToDoTaskTest.delteTask("12");
+		ToDoTaskTest.delteTask("5");
 
 		// THEN: I should be able to see a new up to date List name
 
 		Integer taskdeleteAfter = driver.findElements(By.xpath("//*[@id=\"todoID\"]/p")).size();
 
-		int deleteResults = (deleteTaskBefore);
+		
+		int deleteResults = (deleteTaskBefore - 1);
 
 		// ASSERTION
-
-		if (taskdeleteAfter.equals(taskdeleteAfter)) {
+		
+		if(deleteTaskBefore.equals(deleteResults)) {
 			test.log(LogStatus.PASS, "User was deleted");
 		} else {
 			test.log(LogStatus.FAIL, "user was not Updated");
@@ -250,12 +272,16 @@ public class AutomatedTesting {
 
 		assertEquals(taskdeleteAfter, deleteResults);
 
-	}
 
+	}
+	
+	
+	
+	
 	// CREATE A NEW LIST ITEM
 	@Test
 	public void createAList() throws InterruptedException {
-
+		
 		test = report.startTest("CREATE LIST TEST");
 
 		// GIVEN: That I can access the website
@@ -281,8 +307,8 @@ public class AutomatedTesting {
 		Integer resultList = initalListCount + 1;
 
 		// ASSERTION
-
-		if (expectedList.equals(resultList)) {
+		
+		if(expectedList.equals(resultList)) {
 			test.log(LogStatus.PASS, "User was deleted");
 		} else {
 			test.log(LogStatus.FAIL, "user was not Updated");
@@ -295,7 +321,7 @@ public class AutomatedTesting {
 	// UPDATE A NEW LIST ITEM
 	@Test
 	public void updateLIST() throws InterruptedException {
-
+		
 		test = report.startTest("UPDATE LIST TEST");
 
 		// GIVEN: That I can access the website
@@ -303,11 +329,9 @@ public class AutomatedTesting {
 		driver.get(URL);
 
 		// WHEN: I click on create list button on my index page
-
+		
 		website.updateListButton();
-
-		targ = driver.findElement(By.xpath("//*[@id=\"ListName\"]/p[1]"));
-
+		
 		// THEN: I create a LIST using information
 
 		ToDoListTest.updateList("1", "UPDATE TEST");
@@ -317,14 +341,15 @@ public class AutomatedTesting {
 		// THEN: I should be able to see a new up to date List name
 
 		WebElement targtwo = driver.findElement(By.xpath("//*[@id=\"ListName\"]/p[1]"));
-
-		String newValue = targtwo.getText(); // "Work"
-
-		if (expected.equals(newValue)) {
+		
+		String newValue = targtwo.getText(); // "UPDATE TEST""
+		
+		if(expected.equals(newValue)) {
 			test.log(LogStatus.PASS, "User was deleted");
 		} else {
 			test.log(LogStatus.FAIL, "user was not Updated");
 		}
+		
 
 		// ASSERTION
 		assertEquals(expected, newValue);
@@ -334,13 +359,13 @@ public class AutomatedTesting {
 	// DELETE A NEW LIST ITEM
 	@Test
 	public void deletList() throws InterruptedException {
-
+		
 		test = report.startTest("DELETE LIST TEST");
 
 		// GIVEN: That I can access the website
 
 		driver.get(URL);
-
+		
 		// THEN I WANT TO CHECK THE INITAL VALUE AMOUNT OF LIST ITEMS
 
 		Integer deleteInitalCount = driver.findElements(By.xpath("//*[@id=\"todoID\"]/p")).size();
@@ -349,37 +374,41 @@ public class AutomatedTesting {
 
 		website.deleteListButton();
 
+
 		// THEN: I create a LIST using information
 
-		ToDoListTest.delteList("6");
+		ToDoListTest.delteList("2");
 
 		// THEN: I should be able to see a new up to date List name
 
-		Integer aftercount = driver.findElements(By.xpath("//*[@id=\"todoID\"]/p")).size();
-
+	
+		
 		Integer deleteResults = deleteInitalCount - 1;
 
+		Integer aftercount = driver.findElements(By.xpath("//*[@id=\"todoID\"]/p")).size();
+		
 		// ASSERTION
 
-		if (aftercount.equals(deleteResults)) {
+		if(aftercount.equals(deleteResults)) {
 			test.log(LogStatus.PASS, "User was deleted");
 		} else {
 			test.log(LogStatus.FAIL, "user was not Updated");
 		}
-
+		
 		assertEquals(aftercount, deleteResults);
 
 	}
+	
 
 	@AfterAll
 	public static void tearDown() {
-
+		
 		driver.close();
-
-		// clean up extent report
+		
+		//clean up extent report
 		report.flush();
 		report.close();
-
+	
 	}
 
 }
